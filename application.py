399547cbfedda21f1e008d10c25581a2a7d3149f
Harry Hanskat <harry.e.hanskat@xcelenergy.com>
2021-11-16 11:21:21 -0600

    created application file and branch prior to rebasing

---
+++ application.py
@@ -0,0 +1,2 @@
+print("This is the application file")
+print("this should be the latest version")
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print("This is the main class")
 
-print("Delete this line when trying to rebase")
+print("this is the updated main with application file added")
